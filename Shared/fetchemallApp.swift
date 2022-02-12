30ba0b48638861c63117fe83f48948d5049d419d
Jakub Konka <kubkon@jakubkonka.com>
2022-02-12 13:35:03 +0100

    Initial Commit

---
+++ Shared/fetchemallApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchemallApp.swift
+//  Shared
+//
+//  Created by Jakub Konka on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct fetchemallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
